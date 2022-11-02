f536e005412176f8cce0a2efa3fc4e2f0f6229c7
Rika Nishimura <rika@MacBook-Pro.local>
2022-11-02 08:13:51 +0100

    merge formik to main

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,80 +1,68 @@
-import React, { FormEvent, useState } from "react";
+import React from "react";
 import axios from "axios";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Register = () => {
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-    phone: "",
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const fieldName = e.target.name;
-    setUser((prevState) => {
-      return { ...prevState, [fieldName]: e.target.value };
-    });
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const res = await axios.post(
-      "http://localhost:3007/api/users/register",
-      user
-    );
-    console.log(res.data.message);
-    setUser({
+  const formik = useFormik({
+    initialValues: {
       name: "",
       email: "",
       password: "",
       phone: "",
-    });
-  };
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "Must be at least 8 characters")
+        .required("Required"),
+      phone: Yup.string().required("Required"),
+    }),
+    onSubmit: async (values, { resetForm }) => {
+      // alert(JSON.stringify(values, null, 2));
+      console.log(values);
+      await axios.post("http://localhost:3007/api/users/register", values);
+      resetForm({});
+    },
+  });
 
   return (
     <div className="register">
       <h1>Register:</h1>
       <div className="card">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={formik.handleSubmit}>
           <div className="form__section">
             <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={user.name}
-              onChange={handleChange}
-            />
+            <input type="text" id="name" {...formik.getFieldProps("name")} />
+            {formik.touched.name && formik.errors.name ? (
+              <div>{formik.errors.name}</div>
+            ) : null}
           </div>
           <div className="form__section">
             <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={user.email}
-              onChange={handleChange}
-            />
+            <input type="email" id="email" {...formik.getFieldProps("email")} />
+            {formik.touched.email && formik.errors.email ? (
+              <div>{formik.errors.email}</div>
+            ) : null}
           </div>
           <div className="form__section">
             <label htmlFor="password">Password:</label>
             <input
               type="password"
-              name="password"
               id="password"
-              value={user.password}
-              onChange={handleChange}
+              {...formik.getFieldProps("password")}
             />
+            {formik.touched.password && formik.errors.password ? (
+              <div>{formik.errors.password}</div>
+            ) : null}
           </div>
           <div className="form__section">
             <label htmlFor="phone">Phone:</label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              value={user.phone}
-              onChange={handleChange}
-            />
+            <input type="text" id="phone" {...formik.getFieldProps("phone")} />
+            {formik.touched.phone && formik.errors.phone ? (
+              <div>{formik.errors.phone}</div>
+            ) : null}
           </div>
           <div className="form__section">
             <button type="submit">Register</button>
